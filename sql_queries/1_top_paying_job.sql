e23117646b99f82ba5d15793baf96c9e7d4ef269
beanboobi <ansen131@gmail.com>
2026-08-09 18:24:38 +0800

    add more function for query 1

--- sql_queries/1_top_paying_job.sql
+++ sql_queries/1_top_paying_job.sql
@@ -13,3 +13,15 @@ WHERE
 	AND job_location = 'Singapore'
 ORDER BY salary_year_avg DESC
 LIMIT 10;
+
+-- Calculates average salary for each country for Data Analyst roles
+SELECT
+    job_postings_fact.job_country,
+    AVG(job_postings_fact.salary_year_avg) AS avg_salary
+FROM job_postings_fact
+WHERE 
+    job_postings_fact.job_title_short = 'Data Analyst'
+    AND job_postings_fact.salary_year_avg IS NOT NULL
+    AND job_postings_fact.job_country IS NOT NULL
+GROUP BY job_postings_fact.job_country
+ORDER BY job_postings_fact.job_country ASC;
